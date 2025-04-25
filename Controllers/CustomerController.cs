e3a96d1a5309a68637eecdaf2aa579112bf1e7c0
tamasnovak95 <tamas.novak95@gmail.com>
2025-04-25 16:43:17 +0200

    added Put and  Delete

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -34,8 +34,33 @@ namespace CustomerOrderManager.Controllers
             await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(GetAll), new { id = customer.Id }, customer);
         }
-       
 
+       [HttpPut("{id}")]
+public async Task<IActionResult> UpdateCustomer(int id, Customer updatedCustomer)
+{
+    var customer = await _context.Customers.FindAsync(id);
+    if (customer == null)
+        return NotFound();
+
+    customer.Name = updatedCustomer.Name;
+    customer.Email = updatedCustomer.Email;
+    customer.Phone = updatedCustomer.Phone;
+
+    await _context.SaveChangesAsync();
+    return NoContent(); // 204 No Content = success, no return body
+}
+
+        [HttpDelete("{id}")]
+public async Task<IActionResult> DeleteCustomer(int id)
+{
+    var customer = await _context.Customers.FindAsync(id);
+    if (customer == null)
+        return NotFound();
+
+    _context.Customers.Remove(customer);
+    await _context.SaveChangesAsync();
+    return NoContent();
+}
        
     }
 
--- obj/Debug/net8.0/CustomerOrderManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/CustomerOrderManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomerOrderManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e5a8e93a24944435159aea3d0da8c9956aa3c50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+725594558f71321a82f7cb4021b7b7a9f355e3fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomerOrderManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomerOrderManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
